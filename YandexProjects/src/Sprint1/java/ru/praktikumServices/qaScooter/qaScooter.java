cc649a4f4324a971653641e0343305928bccd18e
DenisSangi <dsangi@cloudally.com>
2022-02-15 18:47:55 +0300

    1

---
+++ YandexProjects/src/Sprint1/java/ru/praktikumServices/qaScooter/qaScooter.java
@@ -0,0 +1,8 @@
+package Sprint1.java.ru.praktikumServices.qaScooter;
+
+public class qaScooter {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+    }
+} 
